d352efa67b7093357fdeca07c00477a59deb159a
riv-ryzhkov <riv.ryzhkovgmail.com>
2025-12-14 13:12:46 +0100

    first commit

---
+++ main/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from .views import IndexTab
+
+urlpatterns = [
+    path('', views.index_start, name='start'),
+    path('tabs/', IndexTab.as_view(), name='main'),
+    # path('tabs/', views.index_tab, name='main'),
+    path('books/', views.index, name='home'),
+    path('book/<int:id>/view', views.book_view, name='book_view'),
+    path('book/<int:id>/edit', views.book_edit, name='book_edit'),
+    path('book/<int:id>/delete', views.book_delete, name='book_delete'),
+    path('about/', views.about, name='about'),
+    path('create/', views.create, name='create'),
+
+]
